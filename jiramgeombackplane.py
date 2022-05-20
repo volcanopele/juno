a4dddb2e291ee7688198917cf6538050532006c1
Jason Perry <volcanopele@gmail.com>
2022-05-20 10:54:08 -0700

    jiramgeombackplane now feature complete
    script now generates cubes files, gives them labels, and merges them. I also now using the IFOV from the IK instead of the calculated value

--- jiramgeombackplane.py
+++ jiramgeombackplane.py
@@ -7,6 +7,7 @@ from tkinter import filedialog as fd
 import tkinter as tk
 import os, sys, getopt
 import numpy as np
+import kalasiris as isis
 
 ###
 # This script takes a time in UTC as input (either directly or via JIRAM 
@@ -36,9 +37,12 @@ method = 'Intercept/Ellipsoid'
 method2 = 'ELLIPSOID'
 tdbfmt = 'YYYY MON DD HR:MN:SC.### TDB ::TDB'
 xlsxmt = 'MM/DD/YYYY HR:MN:SC.###'
-useLabel = False
+datefmt = 'YYYY-MM-DD'
+timefmt = 'HR:MN:SC.###'
 
-# initialize functions
+###########################
+### INITALIZE FUNCTIONS ###
+###########################
 
 # fileParse takes an input JIRAM label file and pulls out pieces of information 
 # that will be used by the script, outputting as a tuple the image mid-time, 
@@ -69,6 +73,21 @@ def fileParse(inputs):
 			stopTime = stopTimes[2]
 			stopTime = stopTime.split('"')[1]
 			stopTime = stopTime.split('/')[1]
+		elif 'PRODUCT_CREATION_TIME' in line:
+			productCreate = line
+			productCreates = productCreate.split(" ")
+			productCreates = sorted(productCreates, reverse=True)
+			productCreate = productCreates[2]
+		elif 'SEQUENCE_NUMBER' in line:
+			sequenceNum = line
+			sequenceNums = sequenceNum.split(" ")
+			sequenceNums = sorted(sequenceNums, reverse=True)
+			sequenceNum = sequenceNums[2]
+		elif 'SEQUENCE_SAMPLES' in line:
+			sequenceSam = line
+			sequenceSams = sequenceSam.split(" ")
+			sequenceSams = sorted(sequenceSams, reverse=True)
+			sequenceSam = sequenceSams[2]
 		elif 'PRODUCT_ID ' in line:
 			if line.startswith('PRODUCT_ID ', 0):
 				productID = line
@@ -85,13 +104,13 @@ def fileParse(inputs):
 	etStop = spiceypy.scs2e(-61999,stopTime)
 	# Image mid-time calculated
 	et = (etStart+etStop)/2
-	orbit = "PJ%s"%(orbit)
+	exposureTime = etStop - etStart
 	
 	# close file
 	file.close()
 	
 	# tuple with image mid-time, product ID, and orbit output by function
-	return [et, productID, orbit, etStart]
+	return [et, productID, orbit, etStart, exposureTime, startTime, productCreate, sequenceNum, sequenceSam]
 
 ####################
 ### SCRIPT START ###
@@ -99,15 +118,14 @@ def fileParse(inputs):
 
 # initialize spice files
 spiceypy.furnsh( metakr )
-spiceypy.furnsh( 'io_north_pole.bsp' )
 
 # open file dialog. Select one or more label files as input. Hit cancel if you want to 
 # manually input a time
 
 inputFiles = fd.askopenfilenames(title='Select Labels', filetypes=(('PDS Labels', '*.LBL'), ('All files', '*.*')))
 numFiles = len(inputFiles)
-offsetFile = fd.askopenfilename(title='Select Offset CSV', filetypes=(('CSV Files', '*.LBL'), ('All files', '*.*')))
-numOffsetFile = len(offsetFile)
+offsetInput = fd.askopenfilename(title='Select Offset CSV', filetypes=(('CSV Files', '*.csv'), ('All files', '*.*')))
+numOffsetInput = len(offsetInput)
 
 if numFiles > 0:
 	for file in inputFiles:
@@ -115,10 +133,16 @@ if numFiles > 0:
 		et = parseTuple[0]
 		timstr = spiceypy.timout( et, xlsxmt )
 		etStart = parseTuple[3]
+		exposureTime = parseTuple[4]
+		sclkStart = parseTuple[5]
 		productID = parseTuple[1]
 		orbit = parseTuple[2]
+		productCreate = parseTuple[6]
+		sequenceNum = parseTuple[7]
+		sequenceSam = parseTuple[8]
 		
-		# setup output file
+		# setup output files
+		# first generate the file names for each parameter CSV file
 		root = os.path.dirname(file)
 		name = os.path.basename(file)
 		fileBase = root + '/' + productID
@@ -129,6 +153,7 @@ if numFiles > 0:
 		incidenceFile = fileBase + '_incidence.csv'
 		phaseFile = fileBase + '_phase.csv'
 		
+		# open each CSV file
 		latitudeFile = open( latitudeFile, 'w' )
 		longitudeFile = open( longitudeFile, 'w' )
 		altitudeFile = open( altitudeFile, 'w' )
@@ -136,42 +161,42 @@ if numFiles > 0:
 		incidenceFile = open( incidenceFile, 'w' )
 		phaseFile = open( phaseFile, 'w' )
 		
-		# may not even this part
-		
-		# calculate center spice pixel
 		# obtain cartesian coordinates for sub-spacecraft point at the time of closest approach
 		[spoint, trgepc, subsrfvec] = spiceypy.subpnt( method, target, etStart, tarfrm, abcorr, scname )
-		print(subsrfvec)
 		
 		# get radii of Io from spice
 		[num, radii] = spiceypy.bodvrd(target, 'RADII', 3)
 		
-		# convert cartesian coordinates of lat/lon
-		[radius, lon, lat] = spiceypy.reclat( spoint )
-		
-		# convert lat and lon from radians to degrees
-		lon = lon * spiceypy.dpr()
-		lat = lat * spiceypy.dpr()
-		
-		# convert longitude domain from -180-180 E longitude to 0-360 W longitude
-		if lon <= 0.0:
-			lon = math.fabs(lon)
+		# obtain derived center pixel from derived file, if available
+		if offsetInput != "":
+			offsetFile = open(offsetInput)
+			offsetInfo = offsetFile.readlines()
+			for line in offsetInfo:
+				if productID in line:
+					derivedLine = line.split(",")
+					derivedX = float(derivedLine[1])
+					derivedY = float(derivedLine[2])
+					break
+			offsetFile.close()
 		else:
-			lon = 360.0 - lon
+			derivedY = ""
 		
-		print(lat,lon)
+		##################################
+		######## L-BAND BACKPLANE ########
+		##################################
 		
-		# L-band
 		[shape, frame, bsight, nbounds, bounds] = spiceypy.getfov(lbandfrm, 20)
 		
+		# calculation of the IFOV
 		dy = np.abs((bounds[0,0]-bounds[2,0]) / 128)
 		dx = np.abs((bounds[0,1]-bounds[2,1]) / 432)
-
-		xp = np.arange(0.5,431.51,1)*dx + bounds[3,1]
-		yp = bounds[3,0] - np.arange(0.5,127.51,1)*dy
-		zp = bounds[0,2]
 		
-		# calculate center pixel
+		# IFOV from the IK
+		# for now using this, but I can commment this out
+		dx = 0.000237767
+		dy = 0.000237767
+		
+		# calculate center pixel in the M-Band FOV
 		xform = spiceypy.pxfrm2(tarfrm, lbndnm, trgepc, etStart)
 		lbandsubvec = spiceypy.mxv(xform, subsrfvec)
 		lbandsubvec[0] = lbandsubvec[0] / lbandsubvec[2]
@@ -182,8 +207,21 @@ if numFiles > 0:
 		centerY = lbandsubvec[0] - bounds[3,0]
 		centerY /= dx
 		centerY *= -1
-		print(centerX)
-		print(centerY)
+		
+		# calculating offsets. probably still need this regardless of derivedY values
+		if derivedY != "" and derivedY <= 128:
+			offsetY = centerY - derivedY
+			offsetY *= dy
+			offsetX = centerX - derivedX
+			offsetX *= dx
+		else:
+			offsetY = 0
+			offsetX = 0
+			
+		# generate numpy arrays of radian pixel locations for both the X and Y directions
+		xp = np.arange(0.5,431.51,1)*dx + bounds[3,1] + offsetX
+		yp = bounds[3,0] - np.arange(0.5,127.51,1)*dy + offsetY
+		zp = bounds[0,2]
 		
 		# initialize the arrays for different backplanes
 		llon = np.zeros([432,128])
@@ -200,6 +238,7 @@ if numFiles > 0:
 		lalt.fill(-1024.0)
 		
 		for i in range(0,128):
+			# initialize each line so as to clear the previous line
 			latitudeLine = ""
 			longitudeLine = ""
 			altitudeLine = ""
@@ -240,18 +279,22 @@ if numFiles > 0:
 			print(phaseLine, file = phaseFile)
 			print(incidenceLine, file = incidenceFile)
 		
-		# M-band
+		##################################
+		######## M-BAND BACKPLANE ########
+		##################################
+		
 		[shape, frame, bsight, nbounds, bounds] = spiceypy.getfov(mbandfrm, 20)
 		
-		# calculate the angle difference for one pixel in both X and Y
+		# calculate the IFOV
 		dy = np.abs((bounds[0,0]-bounds[2,0]) / 128)
 		dx = np.abs((bounds[0,1]-bounds[2,1]) / 432)
-
-		xp = np.arange(0.5,431.51,1)*dx + bounds[3,1]
-		yp = bounds[3,0] - np.arange(0.5,127.51,1)*dy
-		zp = bounds[0,2]
 		
-		# calculate center pixel
+		# IFOV from the IK
+		# for now using this, but I can commment this out
+		dx = 0.000237767
+		dy = 0.000237767
+		
+		# calculate center pixel in the M-Band FOV
 		xform = spiceypy.pxfrm2(tarfrm, mbndnm, trgepc, etStart)
 		mbandsubvec = spiceypy.mxv(xform, subsrfvec)
 		mbandsubvec[0] = mbandsubvec[0] / mbandsubvec[2]
@@ -263,8 +306,21 @@ if numFiles > 0:
 		centerY /= dx
 		centerY *= -1
 		centerY += 128
-		print(centerX)
-		print(centerY)
+		
+		# calculating X and Y offsets if derived information is available
+		if derivedY != "" and derivedY >= 129:
+			offsetY = centerY - derivedY
+			offsetY *= dy
+			offsetX = centerX - derivedX
+			offsetX *= dx
+		else:
+			offsetY = 0
+			offsetX = 0
+		
+		# generate numpy arrays for both the X and Y directions
+		xp = np.arange(0.5,431.51,1)*dx + bounds[3,1] + offsetX
+		yp = bounds[3,0] - np.arange(0.5,127.51,1)*dy - offsetY
+		zp = bounds[0,2]
 		
 		# initialize the arrays for different backplanes
 		mlon = np.zeros([432,128])
@@ -281,6 +337,7 @@ if numFiles > 0:
 		malt.fill(-1024.0)
 		
 		for i in range(0,128):
+			# initialize each line so as to clear the previous line
 			latitudeLine = ""
 			longitudeLine = ""
 			altitudeLine = ""
@@ -328,13 +385,123 @@ if numFiles > 0:
 		emissionFile.close()
 		phaseFile.close()
 		incidenceFile.close()
+		
+		# time to merge some of these products into ISIS files if the original image exists
+		image = fileBase + '.IMG'
+		if os.path.exists(image):
+			imageCub = fileBase + '.cub'
+			mirrorCub = fileBase + '.mirror.cub'
+		
+			# convert IMG to ISIS cube and mirror it (to match geometry)
+			isis.raw2isis(from_=image, to_=imageCub, samples_=432, lines_=256, bands_=1, bittype_="REAL")
+			isis.mirror(from_=imageCub, to_=mirrorCub)
+		
+			# add labels to the image file
+			isis.editlab(from_=mirrorCub, opt_="addg", grpname_="Instrument")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="SpacecraftName", value=scname)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="InstrumentId", value="JIR")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="TargetName", value=target)
+			startDate = spiceypy.timout( etStart, datefmt )
+			startTime = spiceypy.timout( etStart, timefmt )
+			startTime = startDate + "T" + startTime
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="StartTime", value=startTime)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="SpacecraftClockStartCount", value=sclkStart)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Instrument", keyword="ExposureDuration", value=exposureTime)
+			isis.editlab(from_=mirrorCub, opt_="addg", grpname_="Archive")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="DataSetId", value="JNO-J-JIRAM-3-RDR-V1.0")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ProductVersionId", value='"01"')
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ProducerId", value="ISTITUTO NAZIONALE DI ASTROFISICA")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ProductId", value=productID)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ProducerName", value="A.ADRIANI - R.NOSCHESE")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ProductCreationTime", value=productCreate)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="FileName", value=productID)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="FocalLength", value=0.1583)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="FNumber", value=3.7)	
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="SequenceNumber", value=sequenceNum)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="SequenceSample", value=sequenceSam)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="ExposureTimestamp", value=sclkStart)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="IFOV", value="2.378e-004")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="IFOVUnit", value="rad/px")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="PixelUnit", value="W/(m^2*sr)")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="FiltersAvailable", value="L-BAND M-BAND")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="FocalLengthUnit", value="M")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="InstrumentType", value="INFRARED IMAGING SPECTROMETER")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="DetectorDescription", value="2D Array")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="PixelHeight", value=38.0)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="PixelHeightUnit", value="MICRON")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="PixelWidth", value=38.0)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="PixelWidthUnit", value="MICRON")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="Archive", keyword="OrbitNumber", value=orbit)
+			isis.editlab(from_=mirrorCub, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="BandBin", keyword="FilterName", value="L-BAND")
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=mirrorCub, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert latitude file to cube
+			latitudeFile = fileBase + '_latitude.csv'
+			latitudeCube = fileBase + '.latitude.cub'
+			isis.ascii2isis(from_=latitudeFile, to_=latitudeCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=latitudeCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=latitudeCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Latitude")
+			isis.editlab(from_=latitudeCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=latitudeCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=latitudeCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert longitude file to cube
+			longitudeFile = fileBase + '_longitude.csv'
+			longitudeCube = fileBase + '.longitude.cub'
+			isis.ascii2isis(from_=longitudeFile, to_=longitudeCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=longitudeCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=longitudeCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Longitude")
+			isis.editlab(from_=longitudeCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=longitudeCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=longitudeCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert altitude file to cube
+			altitudeFile = fileBase + '_altitude.csv'
+			altitudeCube = fileBase + '.altitude.cub'
+			isis.ascii2isis(from_=altitudeFile, to_=altitudeCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=altitudeCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=altitudeCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Altitude")
+			isis.editlab(from_=altitudeCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=altitudeCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=altitudeCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert emission file to cube
+			emissionFile = fileBase + '_emission.csv'
+			emissionCube = fileBase + '.emission.cub'
+			isis.ascii2isis(from_=emissionFile, to_=emissionCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=emissionCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=emissionCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Emission Angle")
+			isis.editlab(from_=emissionCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=emissionCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=emissionCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert incidence file to cube
+			incidenceFile = fileBase + '_incidence.csv'
+			incidenceCube = fileBase + '.incidence.cub'
+			isis.ascii2isis(from_=incidenceFile, to_=incidenceCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=incidenceCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=incidenceCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Incidence Angle")
+			isis.editlab(from_=incidenceCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=incidenceCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=incidenceCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# convert phase file to cube
+			phaseFile = fileBase + '_phase.csv'
+			phaseCube = fileBase + '.phase.cub'
+			isis.ascii2isis(from_=phaseFile, to_=phaseCube, order_="bsq", samples_=432, lines_=256, bands_=1, skip_=0, setnullrange_="true", nullmin_=-2000, nullmax_=-1000)
+			isis.editlab(from_=phaseCube, opt_="addg", grpname_="BandBin")
+			isis.editlab(from_=phaseCube, option="addkey", grpname="BandBin", keyword="FilterName", value="Phase Angle")
+			isis.editlab(from_=phaseCube, option="addkey", grpname="BandBin", keyword="Center", value=3455)
+			isis.editlab(from_=phaseCube, option="addkey", grpname="BandBin", keyword="Width", value=290)
+			isis.editlab(from_=phaseCube, option="addkey", grpname="BandBin", keyword="NaifIkCode", value=lbandfrm)
+		
+			# create merged product
+			fromlist_path = isis.fromlist.make([mirrorCub, latitudeCube, longitudeCube, altitudeCube, phaseCube, incidenceCube, emissionCube])
+			geomCub = fileBase + '.geom.cub'
+			isis.cubeit(fromlist=fromlist_path, to_=geomCub, proplab_=mirrorCub)
+		
 
 spiceypy.unload( metakr )
-spiceypy.unload( 'io_north_pole.bsp' )
-
-# ascii2isis from=JIR_IMG_RDR_2021052T110632_V01.csv to=JIR_IMG_RDR_2021052T110632_V01.latstart.cub order=bsq samples=432 lines=256 bands=1 skip=0 setnullrange=yes nullmin=-2000 nullmax=-1000
-# handmos from=JIR_IMG_RDR_2021052T110632_V01.latstart.cub mosaic=JIR_IMG_RDR_2021052T110632_V01.mirror.cub outband=2 null=true
-# ascii2isis from=JIR_IMG_RDR_2021052T110632_V01_latitude.csv to=JIR_IMG_RDR_2021052T110632_V01.latitude.cub order=bsq samples=432 lines=256 bands=1 skip=0 setnullrange=yes nullmin=-2000 nullmax=-1000
-# ascii2isis from=JIR_IMG_RDR_2021052T110632_V01_longitude.csv to=JIR_IMG_RDR_2021052T110632_V01.longitude.cub order=bsq samples=432 lines=256 bands=1 skip=0 setnullrange=yes nullmin=-2000 nullmax=-1000
-# ascii2isis from=JIR_IMG_RDR_2021052T110632_V01_emission.csv to=JIR_IMG_RDR_2021052T110632_V01.emission.cub order=bsq samples=432 lines=256 bands=1 skip=0 setnullrange=yes nullmin=-2000 nullmax=-1000
-# ascii2isis from=JIR_IMG_RDR_2021052T110632_V01_altitude.csv to=JIR_IMG_RDR_2021052T110632_V01.altitude.cub order=bsq samples=432 lines=256 bands=1 skip=0 setnullrange=yes nullmin=-2000 nullmax=-1000
